aef6d2df3aebafe844d5d4b87ddc7150ab8f21f8
Jonathan <jonajam2002@gmail.com>
2023-05-23 11:15:18 -0400

    agregar auth0

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
 import './App.scss'
 import { Auth0Provider } from '@auth0/auth0-react'
 
